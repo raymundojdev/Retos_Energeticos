e0518cf549cf4ad7172ded00fc7c20883a180a0b
RETOSENERGETICOS <115562084+RETOSENERGETICOS@users.noreply.github.com>
2023-07-18 09:25:18 -0600

    Update ConexionBD.php

--- modelos/ConexionBD.php
+++ modelos/ConexionBD.php
@@ -4,7 +4,7 @@ class ConexionBD{
 
     static public function cBD(){
 
-        $bd = new PDO("mysql:host=localhost:8080;dbname=retose_bd", "root", "");
+        $bd = new PDO("mysql:host=db5013063583.hosting-data.io;dbname=dbs10968092", "dbu2636717", "R4y$%2023_base!+");
 
         $bd -> exec("set names utf8");
 
